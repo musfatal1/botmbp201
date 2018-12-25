be5a9b70d88f394603f432a23711faf27a0ca957
musfatal1 <46099507+musfatal1@users.noreply.github.com>
2018-12-25 09:09:29 +0300

    Update php.php

--- php.php
+++ php.php
@@ -1,101 +1,148 @@
 <!doctype html>
-<html>
+<html lang="en">
   <head>
-	<meta charset="utf-8">
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/css/bootstrap.min.css" integrity="sha384-Zug+QiDoJOrZ5t4lssLdxGhVrurbmBWopoEl+M6BdEfwnCJZtKxi1KgxUyJq13dy" crossorigin="anonymous">
-	<style>
-	body {
-  padding-top: 70px;
-}
-</style>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+    <title>Hello, world!</title>
+	    <!-- Custom styles for this template -->
+    <link href="cover.css" rel="stylesheet">
   </head>
+  <body>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <a class="navbar-brand" href="#">Sohbet Robotu</a>
 
- 
-
-
-
- <body>
-	<nav class="navbar navbar-expand-md fixed-top navbar-dark bg-primary">
-  	<a class="navbar-brand" href="#">Sohbet Robotu</a>
-  	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
-    	<span class="navbar-toggler-icon"></span>
-  	</button>
-
-  	<div class="collapse navbar-collapse" id="tnavbarsExampleDefaul">
-    	<ul class="navbar-nav mr-auto">
-      	<li class="nav-item active">
-        	<a class="nav-link" href="#">Anasayfa <span class="sr-only">(current)</span></a>
-      	</li>
-      	<li class="nav-item">
-        	<a class="nav-link" href="#">Diller</a>
-      	</li>
-      	<li class="nav-item">
-        	<a class="nav-link disabled" href="#">Sohbet Kutusu</a>
-      	</li>
-    	</ul>
-    	<form class="form-inline my-2 my-lg-0">
-      	<input class="form-control mr-sm-2" type="text" placeholder="Ara" aria-label="Ara">
-      	<button class="btn btn-outline-success my-2 my-sm-0" type="submit">Ara</button>
-    	</form>
-  	</div>
-	</nav>
-	<header>
-  	<div class="container">
-    	<h3>Soru Cevapların Listesi</h3>
-  	</div>
-	</header>
+  <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
+    <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Ana Sayfa <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Diller</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link disabled" href="#">Sohbet Kutusu</a>
+      </li>
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Ara..." aria-label="Ara">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Ara</button>
+    </form>
+  </div>
+</nav>
   
+	<br/>
+	<br/>
+    <h1>Soru cevapların listesi:</h1>
+	<div>
+	<br/>
+	
+	<table class="table table-striped">
+  <thead>
+    <tr>
+      <th scope="col">No</th>
+      <th scope="col">Dili</th>
+      <th scope="col">Soru</th>
+      <th scope="col">Cevap</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>PHP</td>
+      <td>Değişken nasıl tanımlanır?</td>
+      <td>Değişkenin başına $ işareti konularak tanımlanır.</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>PHP</td>
+      <td>Dizgelerin birleştirilmesi nasıl gerçekleştirilir?</td>
+      <td>Dizgeler '.' işleci ile birleştirilir.</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td>C</td>
+      <td>Ekrandan kullanıcı girdisi hangi ifade ile alınır?</td>
+      <td>Kullanıcı girdisi 'scanf' ifadesi ile alınır.</td>
+    </tr>
+	<tr>
+      <th scope="row">4</th>
+      <td>C</td>
+      <td>Fonksiyonda 'return' ne anlama gelmektedir?</td>
+      <td>Fonksiyonun dönüş değeri içerdiği anlamına gelmektedir.</td>
+    </tr>
+  </tbody>
+</table>
+
+<br>
+<hr>
+<br>
 
 <?php
-$myData = json_decode(
- '[{"No":"1","Dili":"PHP",
-  "Soru":"Değişken nasıl tanımlanır?",
-  "Cevap":"Değişkenin başına $ konularak tanımlanır",
-},
-{ "No":"2","Dili":"PHP",
- "Soru":"Dizgelerin birleştirilmesi naıl gerçekleştirilir?",
- "Cevap":"Dizgeler . işleci ile birleştirilir.",
-},
-{ "No":"3","Dili":"C",
- "Soru":"Ekrandan kullanıcı girdisi hangi ifade ile alınır?",
- "Cevap":"Kullanıcı girdisi scanf"
-},
-{ "No":"4","Dili":"C",
- "Soru":"Fonksiyonda return ne anlama gelir?",
- "Cevap":"Fonksiyonun dönüş değeri içerdiği anlamına gelmektedir."
-}]',true);
+  // JSON string
+  $someJSON = '[{"no":"1","dili":"PHP","soru":"Değişken nasıl tanımlanır?","cevap":"Değişkenin başına $ işareti konularak tanımlanır."},{"no":"2","dili":"PHP","soru":"Dizgelerin birleştirilmesi nasıl gerçekleştirilir?","cevap":"Dizgeler '.' işleci ile birleştirilir."},{"no":"3","dili":"C","soru":"Ekrandan kullanıcı girdisi hangi ifade ile alınır?","cevap":"Kullanıcı girdisi \'scanf\' ifadesi ile alınır."},{"no":"4","dili":"C","soru":"Fonksiyonda \'return\' ne anlama gelmektedir?","cevap":"Fonksiyonun dönüş değeri içerdiği anlamına gelmektedir."}]';
 
-
-?>
-<table class="table">"
+  
+  echo	'<table class="table table-striped">
   <thead>
-	<tr>
-  	<th scope="col">No</th>
-  	<th scope="col">Dili</th>
-  	<th scope="col">Soru</th>
-  	<th scope="col">Cevap</th>
-	</tr>  	 
+    <tr>
+	  <th scope="col">No</th>
+      <th scope="col">Dili</th>
+      <th scope="col">Soru</th>
+	  <th scope="col">Cevap</th>
+    </tr>
   </thead>
-<tbody>
+  <tbody>';
 
-  <?php
-foreach($myData as $key=>$item)
-{
-      echo'
-            <tr>
-  	<th scope="row">$item["No"]</th>
-  	<td>$item["Dili"]</td>
-  	<td>$item["Soru"] </td>
-  	<td>$item["Cevap"]</td>
-	</tr>
+  // Convert JSON string to Array
+  $someArray = json_decode($someJSON, true);
+  foreach ($someArray as $key => $value) {
+	echo '<tr><th scope="row">' . $value["no"] . '</th>';
+	echo "<td>" . $value["dili"] . "</td><td>" . $value["soru"] . "</td><td>" . $value["cevap"] . "</td></tr>";
   }
-  </tbody>
+
+  
+  
+
+  // Convert JSON string to Object
+  /*
+  $someObject = json_decode($someJSON);
+  //print_r($someObject);      // Dump all data of the Object
+  
+   //$i = 1;
+   foreach($someObject as $key => $value) {
+	echo '<tr>
+      <th scope="row">' . $value->no . '</th>';
+  
+	echo "<td>" . $value->dili . "</td><td>" . $value->soru . "</td><td>" . $value->cevap . "</td>";
+	
+	echo "</tr>";
+	//$i += 1;
+  }
+  */
+  echo '
+    </tbody>
 </table>';
 ?>
-	<!-- Optional JavaScript -->
-	<!-- jQuery first, then Popper.js, then Bootstrap JS -->
-	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/js/bootstrap.min.js" integrity="sha384-a5N7Y/aK3qNeh15eJKGWxsqtnX/wWdSZSKp+81YjTmS15nvnvxKHuzaWwXHDli+4" crossorigin="anonymous"></script>
+
+
+    <footer class="footer">
+      <div class="container">
+        <span class="text-muted">Sohbet robotu ver. 1.0</span>
+      </div>
+    </footer>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
   </body>
-  </html>
+</html>
